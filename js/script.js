70285ddc5b99de8cd5216cb77894cfeef9998588
Diego Feldman <diegofeldman@gmail.com>
2023-10-10 09:38:16 -0300

    cambios contacto

--- js/script.js
+++ js/script.js
@@ -1,20 +1,37 @@
 
 // Validación de formulario
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("formulario").addEventListener('submit', validarFormulario); 
+document.addEventListener('DOMContentLoaded', function () {
+  const formRegistro = document.getElementById('form-registro');
+  formRegistro.addEventListener('submit', function (event) {
+      // Evita que el formulario se envíe automáticamente
+      event.preventDefault();
+
+      // Realiza la validación de los campos aquí
+      const nombre = document.getElementById('nombre').value.trim();
+      const correo = document.getElementById('correo').value.trim();
+      const telefono = document.getElementById('telefono').value.trim();
+      const genero = document.getElementById('genero').value;
+      const imagen = document.getElementById('imagen').value;
+      const fechaNac = document.getElementById('fecha-nac').value;
+      const clave = document.getElementById('clave').value;
+
+      // Puedes agregar más validaciones según tus requisitos
+      if (nombre === '') {
+          alert('Por favor, ingresa tu nombre y apellido.');
+          return;
+      }
+
+      if (correo === '' || !correo.includes('@')) {
+          alert('Por favor, ingresa un correo electrónico válido.');
+          return;
+      }
+
+      if (clave.length < 8) {
+          alert('La contraseña debe tener al menos 8 caracteres.');
+          return;
+      }
+
+      // Si todas las validaciones pasan, puedes enviar el formulario
+      formRegistro.submit();
   });
-  
-  function validarFormulario(evento) {
-    evento.preventDefault();
-    let correo = document.getElementById('correo').value;
-    if(correo.length == 0) {
-      alert('No has escrito nada en el correo');
-      return;
-    }
-    let clave = document.getElementById('clave').value;
-    if (clave.length < 8) {
-      alert('La clave no es válida');
-      return;
-    }
-    this.submit();
-  }
+});
