c75f10c88b19c25757887560a82937f0b725b3e9
Diego Feldman <diegofeldman@gmail.com>
2023-10-10 09:53:50 -0300

    validar form

--- js/script.js
+++ js/script.js
@@ -3,35 +3,36 @@
 document.addEventListener('DOMContentLoaded', function () {
   const formRegistro = document.getElementById('form-registro');
   formRegistro.addEventListener('submit', function (event) {
-      // Evita que el formulario se envíe automáticamente
-      event.preventDefault();
 
-      // Realiza la validación de los campos aquí
-      const nombre = document.getElementById('nombre').value.trim();
-      const correo = document.getElementById('correo').value.trim();
-      const telefono = document.getElementById('telefono').value.trim();
-      const genero = document.getElementById('genero').value;
-      const imagen = document.getElementById('imagen').value;
-      const fechaNac = document.getElementById('fecha-nac').value;
-      const clave = document.getElementById('clave').value;
+    // Evita que el formulario se envíe automáticamente
+    event.preventDefault();
 
-      // Puedes agregar más validaciones según tus requisitos
-      if (nombre === '') {
-          alert('Por favor, ingresa tu nombre y apellido.');
-          return;
-      }
+    // Los campos se validan acá
+    const nombre = document.getElementById('nombre').value.trim();
+    const correo = document.getElementById('correo').value.trim();
+    const telefono = document.getElementById('telefono').value.trim();
+    const genero = document.getElementById('genero').value;
+    const imagen = document.getElementById('imagen').value;
+    const fechaNac = document.getElementById('fecha-nac').value;
+    const clave = document.getElementById('clave').value;
 
-      if (correo === '' || !correo.includes('@')) {
-          alert('Por favor, ingresa un correo electrónico válido.');
-          return;
-      }
+    //Requisitos de validación
+    if (nombre === '') {
+      alert('Por favor, ingresa tu nombre y apellido.');
+      return;
+    }
 
-      if (clave.length < 8) {
-          alert('La contraseña debe tener al menos 8 caracteres.');
-          return;
-      }
+    if (correo === '' || !correo.includes('@')) {
+      alert('Por favor, ingresa un correo electrónico válido.');
+      return;
+    }
 
-      // Si todas las validaciones pasan, puedes enviar el formulario
-      formRegistro.submit();
+    if (clave.length < 8) {
+      alert('La contraseña debe tener al menos 8 caracteres.');
+      return;
+    }
+
+    // Si todas las validaciones pasan, se envía el formulario
+    formRegistro.submit();
   });
 });
